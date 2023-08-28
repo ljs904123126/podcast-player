73e48630fc648cd55abeeb8fadd49b6ad9f19f42
lijiashu <lijiashu@lawyee.com>
2023-08-28 17:49:39 +0800

    first commit

---
+++ src/test/java/org/liaimei/podcast/player/PodcastPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.liaimei.podcast.player;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodcastPlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
